7ca9575c91b9ab62ece704f6c3cf66154ad0aa6e
arturmartins <arturmartinssilva078@gmail.com>
2024-12-14 17:36:03 -0300

    feat(Endereco.java): adiciona classe Endereco com atributos e métodos de acesso

---
+++ src/model/Endereco.java
@@ -0,0 +1,68 @@
+package model;
+
+public class Endereco {
+    public String rua;
+    public int CEP;
+    public int numero;
+    public String bairro;
+    public String cidade;
+    public String estado;
+
+    public Endereco(String rua, int CEP, int numero, String bairro, String cidade, String estado) {
+        this.rua = rua;
+        this.CEP = CEP;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getCEP() {
+        return CEP;
+    }
+
+    public void setCEP(int CEP) {
+        this.CEP = CEP;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+}
